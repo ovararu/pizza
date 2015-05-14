80caf1aa0600109e0e97832b5a2326345f5abf71
Octavian VARARU <octavian.vararu@gmail.com>
2015-05-14 13:50:34 +0300

    Deployment config files - with unicorn

--- Gemfile
+++ Gemfile
@@ -54,3 +54,9 @@ gem 'taps'
 # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
 # gem 'ruby-debug'
 # gem 'ruby-debug19'
+
+# Use unicorn as the app server
+gem 'unicorn'
+
+# Deploy with Capistrano
+gem 'capistrano'
---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/deployer/apps/pizza/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.pizza.sock"
+worker_processes 2
+timeout 30
